780a68e819be22de166edc36e1fab8a6d6b319e3
Bishnupriya Sethi <111557388+Bishnupri@users.noreply.github.com>
2022-08-26 12:27:35 +0530

    Add files via upload

---
+++ invoice.java
@@ -0,0 +1,50 @@
+package com.java;
+import java.util.Scanner;
+
+public class invoice {
+	
+
+	
+	
+	    private int pan;
+	    private String name;
+	    private double taxincome;
+	    private double tax;
+	    
+	    public void input() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter pan number: ");
+	        pan = sc.nextInt();
+	        sc.nextLine();
+	        System.out.print("Enter Name: ");
+	        name = sc.nextLine();
+	        System.out.print("Enter taxable income: ");
+	        taxincome = sc.nextDouble();
+	    }
+	    
+	    public void cal() {
+	        if (taxincome <= 250000)
+	            tax = 0;
+	        else if (taxincome <= 500000)
+	            tax = (taxincome - 250000) * 0.1;
+	        else if (taxincome <= 1000000)
+	            tax = 30000 + ((taxincome - 500000) * 0.2);
+	        else
+	            tax = 50000 + ((taxincome - 1000000) * 0.3);
+	    }
+	    
+	    public void display() {
+	        System.out.println("Pan Number\tName\tTax-Income\tTax");
+	        System.out.println(pan + "\t" + name + "\t" 
+	            + taxincome + "\t" + tax);
+	    }
+	    
+	    public static void main(String args[]) {
+	        invoice obj = new invoice();
+	        obj.input();
+	        obj.cal();
+	        obj.display();
+	    }
+	
+
+}
